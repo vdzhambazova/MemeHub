2798ec01a9146c175c39551f56e1365e459a4a90
veronika.dzhambazova <veronika.dzhambazova@VERONIKADZ-E550>
2017-04-19 10:17:57 +0300

    Add project files.

---
+++ MemeHub.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MemeHub.Web.Startup))]
+namespace MemeHub.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
